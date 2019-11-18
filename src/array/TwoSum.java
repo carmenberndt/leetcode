6bed323b0a9d29be7efbd085c263040e4182b106
Carmen Berndt <cb@ocg.de>
2019-11-18 18:11:05 +0100

    formatting

--- src/array/TwoSum.java
+++ src/array/TwoSum.java
@@ -6,15 +6,12 @@ import java.util.HashMap;
 public class TwoSum {
 
 	/**
-	 * Runtime: 2 ms
-	 * Memory Usage: 38.5 MB
-	 * Time Complexity: O(n)
-	 * Space Complexity: O(n)
+	 * Runtime: 2 ms Memory Usage: 38.5 MB Time Complexity: O(n) Space Complexity: O(n)
 	 */
-	public static int[] twoSum(int[] nums, int target){
+	public static int[] twoSum(int[] nums, int target) {
 		HashMap<Integer, Integer> map = new HashMap<>();
 		// check each element in array
-		for(int i = 0; i < nums.length; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			//calculate remaining value
 			int remain = target - nums[i];
 			if (map.containsKey(remain)) {
