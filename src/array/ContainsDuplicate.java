6420fb9e800ab2b88e36b5af711bf038794214f3
Carmen Berndt <cb@ocg.de>
2019-11-18 11:34:31 +0100

    improved ContainsDuplicate Problem

--- src/array/ContainsDuplicate.java
+++ src/array/ContainsDuplicate.java
@@ -3,18 +3,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Runtime: 9 ms
- * Memory Usage: 44.2 MB
+ * Runtime: 6 ms
+ * Memory Usage: 44 MB
  * Space Complexity: O(n)
  * Time Complexity: O(n)
  */
+@SuppressWarnings("ForLoopReplaceableByForEach")
 public class ContainsDuplicate {
   public static boolean containsDuplicate(int[] nums) {
     Set<Integer> hashSet = new HashSet<>();
-    for (int num : nums) {
-      if (hashSet.contains(num))
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+      if (!hashSet.add(num)) {
         return true;
-      hashSet.add(num);
+      }
     }
     return false;
   }
