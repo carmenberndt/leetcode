8751368149803692af4754fce7fe40b9810e33d9
Carmen Berndt <cb@ocg.de>
2019-11-18 14:44:42 +0100

    PlusOne

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import array.ContainsDuplicate;
 import array.IntersectionOfTwoArraysII;
+import array.PlusOne;
 import array.RotateArray;
 import array.SingleNumber;
 import java.util.Arrays;
@@ -8,9 +9,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		int[] nums1 = new int[]{1, 2, 2, 1};
-		int[] nums2 = new int[]{2, 2};
-		int[] result = IntersectionOfTwoArraysII.intersect(nums1, nums2);
+		int[] nums2 = new int[]{2, 0};
+		int[] nums3 = new int[]{2, 0, 9};
 
-		System.out.println(Arrays.toString(result));
+		int[] result1 = PlusOne.plusOne(nums1);
+		int[] result2 = PlusOne.plusOne(nums2);
+		int[] result3 = PlusOne.plusOne(nums3);
+
+		System.out.println(Arrays.toString(result1));
+		System.out.println(Arrays.toString(result2));
+		System.out.println(Arrays.toString(result3));
+
 	}
 }
--- src/array/PlusOne.java
+++ src/array/PlusOne.java
@@ -1,5 +1,32 @@
 package array;
 
+import java.util.Arrays;
+
 public class PlusOne {
 
+	/**
+	 * Runtime: 0 ms Memory Usage: 35 MB Time Complexity: O(n) Space Complexity: O(n)
+	 */
+	public static int[] plusOne(int[] digits) {
+		int length = digits.length;
+		int lastElement = digits[length - 1];
+		if (lastElement != 9) {
+			digits[length - 1]++;
+			return digits;
+		}
+		int[] result = new int[length + 1];
+		digits[length - 1] = 0;
+		for (int i = length - 2; i >= 0; i--) {
+			if (digits[i] != 9) {
+				digits[i]++;
+				return digits;
+			}
+			digits[i] = 0;
+		}
+		if (digits[0] == 0) {
+			result[0] = 1;
+			return result;
+		}
+		return digits;
+	}
 }
