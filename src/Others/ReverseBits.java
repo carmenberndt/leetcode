c53b8e13bb65bae24bf2c21fb17fbea1a05f2373
Carmen Berndt <cb@ocg.de>
2019-11-25 13:23:42 +0100

    ReverseBits

---
+++ src/Others/ReverseBits.java
@@ -0,0 +1,12 @@
+package Others;
+
+public class ReverseBits {
+
+	/**
+	 * Runtime: 1 ms Memory Usage: 31.1 MB Time Complexity: O(1) Space Complexity: O(1)
+	 */
+	// you need treat n as an unsigned value
+	public int reverseBits(int n) {
+		return Integer.reverse(n);
+	}
+}
