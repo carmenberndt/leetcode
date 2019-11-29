85c556de8a0db56261ecc8064ef0d9d62daa5574
Carmen Berndt <cb@ocg.de>
2019-11-29 11:46:23 +0100

    formatting

--- src/array/RotateImage.java
+++ src/array/RotateImage.java
@@ -3,10 +3,7 @@ package array;
 public class RotateImage {
 
 	/**
-	 * Runtime: 0 ms
-	 * Memory Usage: 36.2 MB
-	 * Time Complexity: O(n)
-	 * Space Complexity: O(1)
+	 * Runtime: 0 ms Memory Usage: 36.2 MB Time Complexity: O(n) Space Complexity: O(1)
 	 */
 	public static void rotate(int[][] matrix) {
 		int n = matrix.length;
@@ -14,9 +11,9 @@ public class RotateImage {
 			for (int j = i; j < n - i - 1; j++) {
 				int tmp = matrix[i][j];
 				matrix[i][j] = matrix[n - j - 1][i];
-				matrix[n - j - 1][i] = matrix[n - i - 1][n- j - 1];
-				matrix[n - i - 1][n - j - 1] = matrix[j][n-i-1];
-				matrix[j][n-i-1] = tmp;
+				matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+				matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+				matrix[j][n - i - 1] = tmp;
 			}
 		}
 	}
